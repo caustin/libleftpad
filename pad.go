c218bb6d25cfbe5e029ee95ad3010e4aa824016d
Chris Austin <chaustin@softlayer.com>
2016-07-07 09:57:41 -0500

    Cleaning up the docs

--- pad.go
+++ pad.go
@@ -1,6 +1,6 @@
 package leftpad
 
-// PadLeft takes the inputs string src and returns a new string with len length
+// PadLeft takes the input string src and returns a string of length 'length'
 // padded by the string padding
 func PadLeft(src, padding string, length int) string {
 	for {
